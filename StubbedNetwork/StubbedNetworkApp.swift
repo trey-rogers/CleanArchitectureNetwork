bdec053ad07445b2579b40e6fbaca199b5cd1e8a
Trey Rogers <rogers24trey@gmail.com>
2024-05-25 10:22:47 -0400

    Initial Commit

---
+++ StubbedNetwork/StubbedNetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  StubbedNetworkApp.swift
+//  StubbedNetwork
+//
+//  Created by Trey Rogers on 5/25/24.
+//
+
+import SwiftUI
+
+@main
+struct StubbedNetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
